96dc8d0468b0be20488e39a41dd6e888a373f772
Alina <lindenalina.al@gmail.com>
2020-09-16 10:52:19 +0100

    added input in form

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 class MortgageForm extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            personalSalary: '',
+            partnerSalary: ''
+        }
+    }
     
     render() {
         return ( 
         
             <form>
                 <input type="text" placeholder="Your annual salary" name="personalSalary" value={this.state.personalSalary}/>
+                <input type="text" placeholder="Your partner's salary" name="partnerSalary" value={this.state.partnerSalary} />
             </form>
             
          );
