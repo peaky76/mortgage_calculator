3fee5198491ab3c9ad54a78fcae7a37b41e31125
Alina <lindenalina.al@gmail.com>
2020-09-16 10:55:47 +0100

    onchange method set up

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -7,15 +7,22 @@ class MortgageForm extends Component {
         this.state = {
             personalSalary: '',
             partnerSalary: ''
-        }
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
     
     render() {
         return ( 
         
             <form>
-                <input type="text" placeholder="Your annual salary" name="personalSalary" value={this.state.personalSalary}/>
-                <input type="text" placeholder="Your partner's salary" name="partnerSalary" value={this.state.partnerSalary} />
+                <input type="number" placeholder="Your annual salary" name="personalSalary" value={this.state.personalSalary} onChange={this.handleChange}/>
+                <input type="number" placeholder="Your partner's salary" name="partnerSalary" value={this.state.partnerSalary} onChange={this.handleChange}/>
             </form>
             
          );
